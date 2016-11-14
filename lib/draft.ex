6d18c1b1b8c5e6656330914dd130e365b470ad79
Tom Kruijsen <tom@tomkruijsen.nl>
2016-11-14 21:39:47 +0100

    Add converting paragraph
    Add a test and function for converting a raw Draft.js document
    to a paragraph.

--- lib/draft.ex
+++ lib/draft.ex
@@ -1,2 +1,17 @@
 defmodule Draft do
+  def to_html(input) do
+    blocks = Poison.Parser.parse!(input)["blocks"]
+    Enum.map(blocks, &process_block/1)
+      |> Enum.join("")
+  end
+
+  defp process_block(%{"type" => "unstyled",
+                      "text" => text,
+                      "key" => _,
+                      "data" => _,
+                      "depth" => _,
+                      "entityRanges" => _,
+                      "inlineStyleRanges" => _}) do
+    "<p>#{text}</p>"
+  end
 end
--- test/draft_test.exs
+++ test/draft_test.exs
@@ -2,7 +2,9 @@ defmodule DraftTest do
   use ExUnit.Case
   doctest Draft
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "generate a <p>" do
+    input = ~s({"entityMap":{},"blocks":[{"key":"9d21d","text":"Hello","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}]})
+    output = "<p>Hello</p>"
+    assert Draft.to_html(input) == output
   end
 end
