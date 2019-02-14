43bb62ceef5d61f412923294020817e08daa520d
Jake Buob <jake@digitalonboarding.com>
2019-02-14 15:35:40 -0600

    `use` to allow custom processors

--- lib/draft.ex
+++ lib/draft.ex
@@ -1,22 +1,29 @@
 defmodule Draft do
-  @moduledoc """
-  Provides functions for parsing DraftJS content.
-  """
+  defmacro __using__(_) do
+    quote do
+      @moduledoc """
+      Provides functions for parsing DraftJS content.
+      """
 
-  @doc """
-  Renders the given DraftJS input as html.
+      @doc """
+      Renders the given DraftJS input as html.
 
-  ## Examples
+      ## Examples
       iex> draft = %{"entityMap"=>%{},"blocks"=>[%{"key"=>"1","text"=>"Hello","type"=>"unstyled","depth"=>0,"inlineStyleRanges"=>[],"entityRanges"=>[],"data"=>%{}}]}
       iex> Draft.to_html draft
       "<p>Hello</p>"
-  """
-  def to_html(input) do
-    entity_map = Map.get(input, "entityMap")
+      """
 
-    input
-      |> Map.get("blocks")
-      |> Enum.map(&(Draft.Block.to_html(&1, entity_map)))
-      |> Enum.join("")
+      use Draft.Block
+
+      def to_html(input) do
+        entity_map = Map.get(input, "entityMap")
+
+        input
+        |> Map.get("blocks")
+        |> Enum.map(&(process_block(&1, entity_map)))
+        |> Enum.join("")
+      end
+    end
   end
 end
--- test/draft_test.exs
+++ test/draft_test.exs
@@ -1,65 +1,65 @@
 defmodule DraftTest do
   use ExUnit.Case
-  doctest Draft
+  use Draft
 
   test "generate a <p>" do
     input = %{"entityMap"=>%{},"blocks"=>[%{"key"=>"9d21d","text"=>"Hello","type"=>"unstyled","depth"=>0,"inlineStyleRanges"=>[],"entityRanges"=>[],"data"=>%{}}]}
     output = "<p>Hello</p>"
-    assert Draft.to_html(input) == output
+    assert to_html(input) == output
   end
 
   test "generate a <h1>" do
     input = %{"entityMap"=>%{},"blocks"=>[%{"key"=>"9d21d","text"=>"Hello","type"=>"header-one","depth"=>0,"inlineStyleRanges"=>[],"entityRanges"=>[],"data"=>%{}}]}
     output = "<h1>Hello</h1>"
-    assert Draft.to_html(input) == output
+    assert to_html(input) == output
   end
 
   test "generate a <h2>" do
     input = %{"entityMap"=>%{},"blocks"=>[%{"key"=>"9d21d","text"=>"Hello","type"=>"header-two","depth"=>0,"inlineStyleRanges"=>[],"entityRanges"=>[],"data"=>%{}}]}
     output = "<h2>Hello</h2>"
-    assert Draft.to_html(input) == output
+    assert to_html(input) == output
   end
 
   test "generate a <h3>" do
     input = %{"entityMap"=>%{},"blocks"=>[%{"key"=>"9d21d","text"=>"Hello","type"=>"header-three","depth"=>0,"inlineStyleRanges"=>[],"entityRanges"=>[],"data"=>%{}}]}
     output = "<h3>Hello</h3>"
-    assert Draft.to_html(input) == output
+    assert to_html(input) == output
   end
 
   test "generate a <blockquote>" do
     input = %{"entityMap"=>%{},"blocks"=>[%{"key"=>"9d21d","text"=>"Hello","type"=>"blockquote","depth"=>0,"inlineStyleRanges"=>[],"entityRanges"=>[],"data"=>%{}}]}
     output = "<blockquote>Hello</blockquote>"
-    assert Draft.to_html(input) == output
+    assert to_html(input) == output
   end
 
   test "generate a <br>" do
     input = %{"entityMap"=>%{},"blocks"=>[%{"key"=>"9d21d","text"=>"","type"=>"unstyled","depth"=>0,"inlineStyleRanges"=>[],"entityRanges"=>[],"data"=>%{}}]}
     output = "<br>"
-    assert Draft.to_html(input) == output
+    assert to_html(input) == output
   end
 
   test "wraps single inline style" do
     input = %{"entityMap"=>%{},"blocks"=>[%{"text"=>"Hello","inlineStyleRanges"=>[%{"style"=>"BOLD","offset"=>2,"length"=>2}],"type"=>"unstyled","depth"=>0,"entityRanges"=>[],"data"=>%{},"key"=>"9d21d"}]}
     output = "<p>He<span style=\"font-weight: bold;\">ll</span>o</p>"
-    assert Draft.to_html(input) == output
+    assert to_html(input) == output
   end
 
   test "wraps multiple inline styles" do
     input = %{"entityMap"=>%{},"blocks"=>[%{"text"=>"Hello World!","inlineStyleRanges"=>[%{"style"=>"ITALIC","offset"=>8,"length"=>3},%{"style"=>"BOLD","offset"=>2,"length"=>2}],"type"=>"unstyled","depth"=>0,"entityRanges"=>[],"data"=>%{},"key"=>"9d21d"}]}
     output = "<p>He<span style=\"font-weight: bold;\">ll</span>o Wo<span style=\"font-style: italic;\">rld</span>!</p>"
-    assert Draft.to_html(input) == output
+    assert to_html(input) == output
   end
 
   test "wraps nested inline styles" do
     input = %{"entityMap"=>%{},"blocks"=>[%{"text"=>"Hello World!","inlineStyleRanges"=>[%{"style"=>"ITALIC","offset"=>2,"length"=>5},%{"style"=>"BOLD","offset"=>2,"length"=>2}],"type"=>"unstyled","depth"=>0,"entityRanges"=>[],"data"=>%{},"key"=>"9d21d"}]}
     output = "<p>He<span style=\"font-style: italic; font-weight: bold;\">ll</span><span style=\"font-style: italic;\">o W</span>orld!</p>"
-    assert Draft.to_html(input) == output
+    assert to_html(input) == output
   end
 
   test "wraps overlapping inline styles" do
     input = %{"entityMap"=>%{},"blocks"=>[%{"text"=>"Hello World!","inlineStyleRanges"=>[%{"style"=>"ITALIC","offset"=>2,"length"=>5}, %{"style"=>"BOLD","offset"=>4,"length"=>5}],"type"=>"unstyled","depth"=>0,"entityRanges"=>[],"data"=>%{},"key"=>"9d21d"}]}
     output = "<p>He<span style=\"font-style: italic;\">ll</span><span style=\"font-style: italic; font-weight: bold;\">o W</span><span style=\"font-weight: bold;\">or</span>ld!</p>"
-    assert Draft.to_html(input) == output
+    assert to_html(input) == output
   end
 
   test "wraps anchor entities" do
@@ -68,7 +68,7 @@ defmodule DraftTest do
                             %{"offset"=>2,"length"=>3,"key"=>0}
                           ],"data"=>%{},"key"=>"9d21d"}]}
     output = "<p>He<a href=\"http://google.com\">llo</a> World!</p>"
-    assert Draft.to_html(input) == output
+    assert to_html(input) == output
   end
 
   test "wraps overlapping entities and inline styles" do
@@ -85,6 +85,6 @@ defmodule DraftTest do
                            "depth"=>0,
                            "data"=>%{},"key"=>"9d21d"}]}
     output = "<p><span style=\"font-style: italic;\">He</span><a href=\"http://google.com\"><span style=\"font-style: italic;\">ll</span><span style=\"font-weight: bold;\">o</span></a><span style=\"font-weight: bold;\"> Wo</span>rld!</p>"
-    assert Draft.to_html(input) == output
+    assert to_html(input) == output
   end
 end
